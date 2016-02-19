b084e02dd2cf7b492c69090b6acd548066c7c34f
admin-rgbconsulting <odoo@rgbconsulting.com>
2016-02-19 13:54:09 +0100

    [FIX] Check if quants are moved and pass moves to done to avoid duplication

--- pos_picking_state_fix/models/pos_picking.py
+++ pos_picking_state_fix/models/pos_picking.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # See README file for full copyright and licensing details.
 
+import time
 from openerp import models, api
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class PosPicking(models.Model):
@@ -12,8 +14,12 @@ class PosPicking(models.Model):
         try:
             super(PosPicking, self).create_picking()
         except:
-            # Cancel move lines
             if self.picking_id.state != 'done':
                 for move in self.picking_id.move_lines:
-                    move.do_unreserve()
+                    if move.quant_ids:
+                        # We pass this move to done because the quants were already moved
+                        move.write({'state': 'done', 'date': time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)})
+                    else:
+                        # If there are no moved quants we pass the move to Waiting Availability
+                        move.do_unreserve()
         return True
