d1e26c17c271493a1218776040c47a80fabdbf7a
Gelu <gelu.boros@rgbconsulting.com>
2017-02-16 13:24:51 +0100

    [MIG] pos_order_taxes module

---
+++ pos_order_taxes/models/pos_order_tax.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# See README file for full copyright and licensing details.
+
+from odoo import models, fields
+
+import odoo.addons.decimal_precision as dp
+
+
+class PosOrderTax(models.Model):
+    _name = 'pos.order.tax'
+
+    pos_order = fields.Many2one('pos.order', string='POS Order', ondelete='cascade', index=True)
+    tax = fields.Many2one('account.tax', string='Tax')
+    name = fields.Char(string='Tax Description', required=True)
+    base = fields.Float(string='Base', digits=dp.get_precision('Account'))
+    amount = fields.Float(string='Amount', digits=dp.get_precision('Account'))
